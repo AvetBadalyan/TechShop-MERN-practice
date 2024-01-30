6680ee82222327bf2057548929d9b5e5f7a87edb
AvetBadalyan <avetbadalyan@gmail.com>
2024-01-30 20:19:18 +0400

    add bootstrap and create header and footer with bootstrap components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
-  return <h1>Welcome to Avet's techShop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Avet's TechShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
