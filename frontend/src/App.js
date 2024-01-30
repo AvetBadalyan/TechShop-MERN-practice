b560adc3640b06a84ef6d47320321ec1b5efc054
AvetBadalyan <avetbadalyan@gmail.com>
2024-01-30 21:18:57 +0400

    create home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import HomePage from "./Pages/HomePage/HomePage";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <h1>Welcome to Avet's TechShop</h1>
+          <HomePage />
         </Container>
       </main>
       <Footer />
