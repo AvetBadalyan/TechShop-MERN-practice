e6da2ef7581274b1bf180dd6624e1476387d08b4
AvetBadalyan <avetbadalyan@gmail.com>
2024-01-31 11:40:58 +0400

    add react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import HomePage from "./Pages/HomePage/HomePage";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
@@ -10,8 +10,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Avet's TechShop</h1>
-          <HomePage />
+          <Outlet />
         </Container>
       </main>
       <Footer />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/bootstrap.custom.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import HomePage from "./Pages/HomePage/HomePage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomePage />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
