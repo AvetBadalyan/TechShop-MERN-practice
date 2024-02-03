298ad137c722fb61b4bb8324453808a6c9e83384
AvetBadalyan <avetbadalyan@gmail.com>
2024-02-04 00:17:20 +0400

    add protect and admin middleware with cookies

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRouts.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
