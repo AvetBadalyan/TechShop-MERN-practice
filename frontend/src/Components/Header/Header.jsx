9af8fbeb18caa211af78dc34726506cf47a15bc1
AvetBadalyan <avetbadalyan@gmail.com>
2024-02-09 00:06:36 +0400

    update readme

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>TechShop</Navbar.Brand>
+            <Navbar.Brand>Avet's TechShop</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
